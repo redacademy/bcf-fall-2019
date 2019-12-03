1fdb714a1fe8f8a87d58e4a964ac113368c1ee58
Yongmin Kim <yongmin86k@gmail.com>
2019-12-03 15:46:29 -0800

    Create Onboarding components

---
+++ client/js/screens/Onboarding/Onboarding.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Onboarding = () => {
+  return <Text>Hello Onboarding</Text>;
+};
+
+export default Onboarding;
