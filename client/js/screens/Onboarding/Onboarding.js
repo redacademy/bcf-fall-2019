6335c91e30f3748bdb781c13684fc7ca73096e13
Yongmin Kim <yongmin86k@gmail.com>
2019-12-03 16:23:33 -0800

    Add onboarding images

--- client/js/screens/Onboarding/Onboarding.js
+++ client/js/screens/Onboarding/Onboarding.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ImageBackground} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 
-const Onboarding = () => {
-  return <Text>Hello Onboarding</Text>;
+const OnboardingSwiper = () => {
+  return (
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#000',
+          image: (
+            <ImageBackground
+              source={require('../../assets/images/imgOnboarding01.jpg')}
+              style={{width: '100%', height: '100%'}}
+            />
+          ),
+          title: ``,
+          subtitle: '',
+        },
+        {
+          backgroundColor: '#000',
+          image: (
+            <ImageBackground
+              source={require('../../assets/images/imgOnboarding02.jpg')}
+              style={{width: '100%', height: '100%'}}
+            />
+          ),
+          title: ``,
+          subtitle: '',
+        },
+        {
+          backgroundColor: '#000',
+          image: (
+            <ImageBackground
+              source={require('../../assets/images/imgOnboarding03.jpg')}
+              style={{width: '100%', height: '100%'}}
+            />
+          ),
+          title: ``,
+          subtitle: '',
+        },
+      ]}
+    />
+  );
 };
 
-export default Onboarding;
+export default OnboardingSwiper;
