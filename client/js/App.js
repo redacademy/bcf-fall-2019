c9af841702643ba92ee4ee494ee49fe566abfb60
Yongmin Kim <yongmin86k@gmail.com>
2019-12-06 10:58:55 -0800

    Create Apollo client. Add Apollo Provider

--- client/js/App.js
+++ client/js/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import {ApolloProvider} from 'react-apollo';
+import client from './config/api';
+
 import RootStackNavigator from '../js/navigation/RootStackNavigator';
 
 import SignIn from './screens/SignIn';
 
 export default class App extends Component {
   render() {
-    return <SignIn />;
+    return (
+      <ApolloProvider client={client}>
+        <SignIn />
+      </ApolloProvider>
+    );
     // return <RootStackNavigator />;
   }
 }
