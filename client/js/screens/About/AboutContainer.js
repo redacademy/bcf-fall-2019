1339142548f43f338fed27c0a265b59d333d285c
MattWaraich <mattsinghwaraich@gmail.com>
2019-12-09 10:27:19 -0800

    Bug fix for modal

--- client/js/screens/About/AboutContainer.js
+++ client/js/screens/About/AboutContainer.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import About from './About';
+// import {prisma} from './generated/prisma-client';
 
 class AboutContainer extends Component {
   render() {
-    return <About />;
+    return (
+      <Query query={QUERY_}>
+        {({loading, error, data}) => {
+          if (loading) {
+            return <Loader />;
+          }
+
+          if (error) {
+            return <Text>{error.message}</Text>;
+          }
+
+          if (data) {
+            return <About data={data} />;
+          }
+        }}
+      </Query>
+    );
   }
 }
 
