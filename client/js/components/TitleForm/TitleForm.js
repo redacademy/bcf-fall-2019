9caa68c87e40352558e48273f80006adb1f9e174
Yongmin Kim <yongmin86k@gmail.com>
2019-12-09 11:40:11 -0800

    Create TitleForm components

---
+++ client/js/components/TitleForm/TitleForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import styles from './styles';
+
+const TitleForm = props => {
+  return <Text>{props.children}</Text>;
+};
+
+export default TitleForm;
