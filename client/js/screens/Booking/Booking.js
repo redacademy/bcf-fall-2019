2360ef01d04f7e4ec8f00c56dbcfb1dfba7aa522
Alex Faissal <alefaissal@gmail.com>
2019-12-09 15:51:37 -0800

    Changes on Single Audio Screen

--- client/js/screens/Booking/Booking.js
+++ client/js/screens/Booking/Booking.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 
 const Booking = props => {
-  const Booking = props => {
-    return <Text>Hello Booking</Text>;
-  };
+  return <Text>Hello Booking</Text>;
 };
 
 export default Booking;
