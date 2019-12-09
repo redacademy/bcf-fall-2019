5f2062fd8e0d5185e5e85e71536825cf57f713a0
Yongmin Kim <yongmin86k@gmail.com>
2019-12-09 10:31:00 -0800

    Create Signup screen folder

---
+++ client/js/screens/SignUp/SignUp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const SignUp = props => {
+  return <View>SignUp Screen</View>;
+};
---
+++ client/js/screens/SignUp/SignUpContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import SignUp from './SignUp';
+
+class SignUpContainer extends Component {
+  render() {
+    return <SignUp />;
+  }
+}
+
+export default SignUpContainer;
