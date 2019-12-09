aeb812445dd2ce22925c2638e402a16fdfae9f0c
Yongmin Kim <yongmin86k@gmail.com>
2019-12-09 13:28:00 -0800

    Update TitleForm component

--- client/js/screens/SignUp/SignUp.js
+++ client/js/screens/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
 import {ScrollView, Text, Image, View} from 'react-native';
 import TitleText from '../../components/TitleText';
+import TitleForm from '../../components/TitleForm';
 import styles from './styles';
 import {THEME} from '../../config';
 
@@ -20,6 +21,7 @@ const SignUp = props => {
         <Text style={styles.text}>
           Sign up to see what is happening near you
         </Text>
+        <TitleForm>Profile</TitleForm>
       </SafeAreaView>
     </ScrollView>
   );
