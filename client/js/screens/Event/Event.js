b567993d654a15ce84315c6d672081327d3964e2
MattWaraich <mattsinghwaraich@gmail.com>
2019-12-11 11:21:08 -0800

    Bug fix for bookings page

---
+++ client/js/screens/Event/Event.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Event = props => {
+  return <Text>Hello Event</Text>;
+};
+
+export default Event;
---
+++ client/js/screens/Event/EventContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Event from './Event';
+
+class EventContainer extends Component {
+  render() {
+    <Event />;
+  }
+}
+
+export default EventContainer;
