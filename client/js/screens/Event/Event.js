2f7bdb5dbcdb73ac64c7cf3286432576c82a7d6f
MattWaraich <mattsinghwaraich@gmail.com>
2019-12-11 15:48:54 -0800

    Hardcode event page

--- client/js/screens/Event/Event.js
+++ client/js/screens/Event/Event.js
@@ -1,17 +1,88 @@
 import React from 'react';
-import {View, SafeAreaView, Image} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  ScrollView,
+  Button,
+} from 'react-native';
 import styles from './styles';
 
 const Event = props => {
   return (
     <SafeAreaView>
-      <View>
-        <Image
-          style={styles.eventImg}
-          source={require('../../assets/images/imgHomeCatFamily.png')}
-        />
-        <View style={styles.overlay} />
-      </View>
+      <ScrollView>
+        <View style={styles.eventImg}>
+          <Image
+            style={styles.eventImg}
+            source={require('../../assets/images/imgHomeCatFamily.png')}
+          />
+
+          <View style={styles.overlay}>
+            <Text>Beaver Lake Tour</Text>
+            <Text>Stanley Park</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text>Date/Time</Text>
+          <TextInput
+            style={styles.eventTextInput}
+            onChangeText={text => onChangeText(text)}
+          />
+          <Text>Location</Text>
+          <TextInput
+            style={styles.eventTextInput}
+            onChangeText={text => onChangeText(text)}
+          />
+        </View>
+
+        <View>
+          <Text>Difficulty</Text>
+          <TextInput
+            style={styles.eventTextInputSmall}
+            onChangeText={text => onChangeText(text)}
+          />
+          <Text>Language</Text>
+          <TextInput
+            style={styles.eventTextInputSmall}
+            onChangeText={text => onChangeText(text)}
+          />
+        </View>
+
+        <View>
+          <Text>Event Details</Text>
+          <Text>
+            Come join us as we see take a scenic ferry ride to stanley park, the
+            gem of the Islands. We will then walk off the ferry and hike to
+            where on a clear day you will have views of stanley park Afterwards,
+            we will explore…(Read More)
+          </Text>
+        </View>
+
+        <View>
+          <Text>Map goes here</Text>
+          <Text>View Map</Text>
+        </View>
+
+        <View>
+          <Text>Your Host</Text>
+          <Text>Name</Text>
+          <TextInput
+            style={styles.eventTextInputSmall}
+            onChangeText={text => onChangeText(text)}
+          />
+          <Text>Bio</Text>
+          <TextInput
+            style={styles.eventTextInputSmall}
+            onChangeText={text => onChangeText(text)}
+          />
+        </View>
+
+        <Button style={styles.eventContactButton} title="Contact" />
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- client/js/screens/Event/styles.js
+++ client/js/screens/Event/styles.js
@@ -7,6 +7,32 @@ const styles = StyleSheet.create({
     width: '100%',
     height: width,
   },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    width: '100%',
+    height: 100,
+  },
+  eventTextInput: {
+    height: 36,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '90%',
+  },
+  eventTextInputSmall: {
+    height: 36,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '40%',
+  },
+  eventContactButton: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#F1785B',
+    color: 'red',
+  },
 });
 
 export default styles;
