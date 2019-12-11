bf377767cff34dcb59f39ce9b5aeb3b3c4767f8c
MattWaraich <mattsinghwaraich@gmail.com>
2019-12-10 16:30:49 -0800

    Complete About page styling

--- client/js/screens/About/About.js
+++ client/js/screens/About/About.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {ScrollView, Text, View, Image} from 'react-native';
+import {ScrollView, Text, View, Image, TouchableOpacity} from 'react-native';
 import styles from './styles';
+import ButtonDefault from '../../components/ButtonDefault';
 
 const About = ({info}) => {
   return (
-    <ScrollView>
-      <Text>About Us</Text>
-      <View>
-        <Text>{info.about}</Text>
+    <ScrollView
+      style={styles.aboutPage}
+      contentContainerStyle={{justifyContent: 'space-between'}}>
+      <Text style={styles.aboutTitle}>About Us</Text>
+
+      <View style={styles.aboutDescription}>
+        <Text style={styles.parOne}>
+          BC’s world-class parks are not only the backbone of our tourism
+          economy, but also serve as a haven for family time, recreation,
+          education, inspiration, culture and personal fulfillment. In
+          2018/2019, BC’s parks had over 26 million visits – an increase of
+          nearly five million over five years.
+        </Text>
+        <Text style={styles.parTwo}>
+          Discover Parks brings together nature, art, technology and culture to
+          connect people to nature. Park ambassadors, self guided trails and
+          other new activations will help spread the word about stewardship and
+          provide engaging experiences for park visitors.
+        </Text>
       </View>
-      <Image source={require('../../assets/images/imgAboutUsLogo.png')} />
+
+      <View style={styles.aboutImage}>
+        <Image source={require('../../assets/images/imgAboutUsLogo.png')} />
+      </View>
+
+      <View>
+        <ButtonDefault title="Donate Today" />
+      </View>
     </ScrollView>
   );
 };
---
+++ client/js/screens/About/styles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  aboutPage: {
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+  },
+  aboutTitle: {
+    fontWeight: '300',
+    color: '#013859',
+    fontSize: 52,
+    paddingBottom: 24,
+  },
+  aboutDescription: {
+    fontFamily: 'Montserrat-Regular',
+  },
+  aboutImage: {
+    paddingTop: 24,
+    paddingBottom: 40,
+  },
+  parOne: {
+    fontSize: 19,
+    marginBottom: 15,
+  },
+  parTwo: {
+    fontSize: 19,
+  },
+});
+
+export default styles;
