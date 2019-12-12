adb521d2c9047787109b0b6f6ef163a2f6701402
Alex Faissal <alefaissal@gmail.com>
2019-12-12 09:20:05 -0800

    Add ImageBackground, change two styles name and add onPress methos

--- client/js/components/SelfGuidedItem/SelfGuidedItem.js
+++ client/js/components/SelfGuidedItem/SelfGuidedItem.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import {TouchableOpacity, Text, View, Image} from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+} from 'react-native';
 
 import styles from './styles';
 
 const SelfGuidedItem = ({selfGuidedItem}) => (
-  <TouchableOpacity>
-    <View>
-      <Text style={styles.mainTitle}>{selfGuidedItem.title}</Text>
+  <TouchableOpacity
+    onPress={() => {
+      if (selfGuidedItem) {
+        return navigation.push('SingleSelfGuided', {
+          itemId: selfGuidedItem.id,
+        });
+      }
+    }}>
+    <ImageBackground
+      source={{uri: selfGuidedItem.image}}
+      style={{width: '100%', height: '100%'}}>
+      <Text style={styles.title}>{selfGuidedItem.title}</Text>
       <Text style={styles.location}>{selfGuidedItem.location}</Text>
       <View>
         <View>
-          <Text style={styles.miniTitles}>Difficulty</Text>
+          <Text style={styles.subTitles}>Difficulty</Text>
           <Text style={styles.infobox}>{selfGuidedItem.difficulty}</Text>
-          <Text style={styles.miniTitles}>Distance</Text>
+          <Text style={styles.subTitles}>Distance</Text>
           <Text style={styles.infobox}>{selfGuidedItem.distance}</Text>
-          <Text style={styles.miniTitles}>Duration</Text>
+          <Text style={styles.subTitles}>Duration</Text>
           <Text style={styles.infobox}>{selfGuidedItem.duration}</Text>
         </View>
         <View>
@@ -28,7 +43,7 @@ const SelfGuidedItem = ({selfGuidedItem}) => (
           />
         </View>
       </View>
-    </View>
+    </ImageBackground>
   </TouchableOpacity>
 );
 
