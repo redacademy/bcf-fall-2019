c73342d906ffd9be7ce1d83b23731d5b75ac80dc
Yongmin Kim <yongmin86k@gmail.com>
2019-12-14 00:46:40 -0800

    Update files related to react-naviagtion

--- client/js/navigation/NavigationLayout.js
+++ client/js/navigation/NavigationLayout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
 
 import HomeScreen from '../screens/Home';
 import SearchScreen from '../screens/Search';
@@ -19,18 +18,21 @@ import Faq from '../screens/Faq';
 import {Image} from 'react-native';
 
 import {sharedNavigationOptions} from './config';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const {black, mediumGrey, white} = Colors;
+import {THEME} from '../config';
 
 const HomeStack = createStackNavigator(
   {
     Home: HomeScreen,
     SelfGuidedTour,
     StoryTrail,
+    About,
+    Contact,
+    Faq,
   },
   {
     defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
       initialRouteName: 'Home',
     }),
   },
@@ -152,13 +154,13 @@ export default createBottomTabNavigator(
       },
     }),
     tabBarOptions: {
-      activeTintColor: black,
-      inactiveTintColor: mediumGrey,
+      activeTintColor: THEME.colors.black,
+      inactiveTintColor: THEME.colors.grey,
       labelStyle: {
         fontSize: 10,
       },
       style: {
-        backgroundColor: white,
+        backgroundColor: THEME.colors.white,
       },
     },
   },
