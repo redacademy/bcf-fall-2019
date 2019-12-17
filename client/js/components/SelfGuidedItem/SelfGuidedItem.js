37de6cece6924bc41116912ef7f4078a5c73a7d1
Alex Faissal <alefaissal@gmail.com>
2019-12-17 14:53:12 -0800

    Add action for save icon in selfguided and singleselfguided screens

--- client/js/components/SelfGuidedItem/SelfGuidedItem.js
+++ client/js/components/SelfGuidedItem/SelfGuidedItem.js
@@ -6,56 +6,81 @@ import {
   Image,
   ImageBackground,
 } from 'react-native';
-
+import SaveEventContext from '../../context/SaveEventContext';
 import styles from './styles';
 
 const SelfGuidedItem = ({selfGuidedItem, navigation}) => (
-  <TouchableOpacity
-    onPress={() => {
-      if (selfGuidedItem) {
-        return navigation.push('SingleSelfGuided', {
-          itemId: selfGuidedItem.id,
-        });
-      }
-    }}
-    style={styles.container}>
-    <ImageBackground
-      source={{uri: selfGuidedItem.image}}
-      style={styles.backgroundImage}>
-      <View style={styles.infoContainer}>
-        <Text style={styles.title}>{selfGuidedItem.title}</Text>
-        <Text style={styles.location}>{selfGuidedItem.location}</Text>
-        <View style={styles.flexRow}>
-          <View style={styles.flexRow}>
-            <View style={styles.flexColumn}>
-              <Text style={styles.subTitles}>Difficulty</Text>
-              <Text style={styles.infobox}>{selfGuidedItem.difficulty}</Text>
-            </View>
-            <View style={styles.flexColumn}>
-              <Text style={styles.subTitles}>Distance</Text>
-              <Text style={styles.infobox}>{selfGuidedItem.distance}</Text>
-            </View>
-            <View style={styles.flexColumn}>
-              <Text style={styles.subTitles}>Duration</Text>
-              <Text style={styles.infobox}>
-                {selfGuidedItem.duration} hours
-              </Text>
+  <SaveEventContext.Consumer>
+    {({savedIds, addSaveId, removeSaveId}) => (
+      <TouchableOpacity
+        onPress={() => {
+          if (selfGuidedItem) {
+            return navigation.push('SingleSelfGuided', {
+              itemId: selfGuidedItem.id,
+            });
+          }
+        }}
+        style={styles.container}>
+        <ImageBackground
+          source={{uri: selfGuidedItem.image}}
+          style={styles.backgroundImage}>
+          <View style={styles.infoContainer}>
+            <Text style={styles.title}>{selfGuidedItem.title}</Text>
+            <Text style={styles.location}>{selfGuidedItem.location}</Text>
+            <View style={styles.flexRow}>
+              <View style={styles.flexRow}>
+                <View style={styles.flexColumn}>
+                  <Text style={styles.subTitles}>Difficulty</Text>
+                  <Text style={styles.infobox}>
+                    {selfGuidedItem.difficulty}
+                  </Text>
+                </View>
+                <View style={styles.flexColumn}>
+                  <Text style={styles.subTitles}>Distance</Text>
+                  <Text style={styles.infobox}>{selfGuidedItem.distance}</Text>
+                </View>
+                <View style={styles.flexColumn}>
+                  <Text style={styles.subTitles}>Duration</Text>
+                  <Text style={styles.infobox}>
+                    {selfGuidedItem.duration} hours
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.flexRow}>
+                <Image
+                  style={styles.contactSocialMediaIcon}
+                  source={require('../../assets/images/icFaveShareDefault.png')}
+                />
+                <TouchableOpacity
+                  onPress={async e => {
+                    try {
+                      if (
+                        savedIds.some(savedId => savedId === selfGuidedItem.id)
+                      ) {
+                        removeSaveId(selfGuidedItem.id);
+                      } else {
+                        addSaveId(selfGuidedItem.id);
+                      }
+                    } catch (error) {
+                      console.log(error);
+                    }
+                  }}>
+                  <Image
+                    style={styles.contactSocialMediaIcon}
+                    source={
+                      savedIds.some(savedId => savedId === selfGuidedItem.id)
+                        ? require('../../assets/images/icFaveCircleActive.png')
+                        : require('../../assets/images/icFaveCircleDefault.png')
+                    }
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-          <View style={styles.flexRow}>
-            <Image
-              style={styles.contactSocialMediaIcon}
-              source={require('../../assets/images/icFaveShareDefault.png')}
-            />
-            <Image
-              style={styles.contactSocialMediaIcon}
-              source={require('../../assets/images/icFaveCircleDefault.png')}
-            />
-          </View>
-        </View>
-      </View>
-    </ImageBackground>
-  </TouchableOpacity>
+        </ImageBackground>
+      </TouchableOpacity>
+    )}
+  </SaveEventContext.Consumer>
 );
 
 export default SelfGuidedItem;
