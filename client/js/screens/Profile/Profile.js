bc45ae662e14d0d2c2ba5254be9071d13998761f
Yongmin Kim <yongmin86k@gmail.com>
2019-12-18 10:33:18 -0800

    Add dynamic header to Profile screen

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -1,8 +1,89 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  Animated,
+  FlatList,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {VibrancyView} from '@react-native-community/blur';
+import styles from './styles';
+import {THEME} from '../../config';
+import PropTypes from 'prop-types';
 
-const Profile = props => {
-  return <Text>Hello Profile</Text>;
+const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView);
+
+const Profile = ({collapsible, onSwitchTheme, headerHeight}) => {
+  const _headerHeight = collapsible.paddingHeight
+    ? collapsible.paddingHeight
+    : headerHeight;
+  const [isAnimated, setAnimated] = useState(false);
+  const [headerAnimation, setHeaderAnimation] = useState(
+    new Animated.ValueXY({x: 0, y: -_headerHeight}),
+  );
+  const {paddingHeight, animatedY} = collapsible;
+
+  const onScroll = e => {
+    const offset = e.nativeEvent.contentOffset;
+    if (offset.y > _headerHeight && !isAnimated) {
+      setAnimated(true);
+      animateHeader();
+      onSwitchTheme(isAnimated);
+    } else if (offset.y <= _headerHeight && isAnimated) {
+      setAnimated(false);
+      animateHeader();
+      onSwitchTheme(isAnimated);
+    }
+  };
+
+  const animateHeader = () => {
+    Animated.timing(headerAnimation, {
+      duration: 500,
+      toValue: isAnimated ? {x: 0, y: -_headerHeight} : {x: 1, y: 0},
+    }).start();
+  };
+
+  return (
+    <>
+      {!paddingHeight && <View style={{height: _headerHeight}} />}
+
+      <AnimatedScrollView
+        scrollEventThrottle={32}
+        onScroll={Animated.event([{nativeEvent: {contentOffset: {y: 0}}}], {
+          useNativeDriver: true,
+          listener: onScroll,
+        })}
+        _mustAddThis={animatedY}
+        contentContainerStyle={{
+          paddingTop: paddingHeight,
+          paddingBottom: THEME.spacing.default(3),
+        }}
+        scrollIndicatorInsets={{
+          top: paddingHeight / 2,
+          bottom: THEME.spacing.default(0.5),
+        }}>
+        <Text>Hello Profile</Text>
+      </AnimatedScrollView>
+
+      <Animated.View
+        style={{
+          ...styles.dynamicHeader,
+          opacity: headerAnimation.x,
+          top: headerAnimation.y,
+          height: paddingHeight || _headerHeight,
+        }}>
+        <VibrancyView blurType="dark" blurAmount={2} style={styles.header} />
+      </Animated.View>
+    </>
+  );
 };
 
 export default Profile;
+
+Profile.propTypes = {
+  collapsible: PropTypes.object,
+  onSwitchTheme: PropTypes.func,
+  headerHeight: PropTypes.number,
+};
--- client/js/screens/Profile/ProfileContainer.js
+++ client/js/screens/Profile/ProfileContainer.js
@@ -1,10 +1,82 @@
 import React, {Component} from 'react';
 import Profile from './Profile';
+import {TouchableOpacity, Image, StatusBar} from 'react-native';
+import {withCollapsible} from 'react-navigation-collapsible';
+import {THEME} from '../../config';
 
 class ProfileContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      headerHeight: 88,
+    };
+  }
+
+  static navigationOptions = ({navigation}) => {
+    const themeColor = navigation.getParam('themeColor') || 'light';
+
+    return {
+      headerBackTitle: true,
+      headerTitleStyle: {
+        color:
+          themeColor === 'light'
+            ? THEME.colors.astronautBlue
+            : THEME.colors.white,
+      },
+      headerTransparent: true,
+      headerStyle: {
+        backgroundColor: 'transparent',
+      },
+
+      headerBackground: () => {
+        return (
+          <StatusBar
+            barStyle={themeColor === 'light' ? 'dark-content' : 'light-content'}
+          />
+        );
+      },
+
+      headerRight: () => (
+        <TouchableOpacity
+          style={{marginRight: 12}}
+          onPress={() => {
+            navigation.toggleDrawer();
+          }}>
+          <Image
+            source={
+              themeColor === 'light'
+                ? require('../../assets/images/icMenuDefault.png')
+                : require('../../assets/images/icMenuWhite.png')
+            }
+            name="burger-menu"
+          />
+        </TouchableOpacity>
+      ),
+    };
+  };
+
+  onSwitchTheme = (Bool = true) => {
+    setTimeout(() => {
+      this.props.navigation.setParams({
+        themeColor: Bool ? 'light' : 'dark',
+      });
+    }, 250);
+  };
+
   render() {
-    return <Profile />;
+    const {collapsible} = this.props;
+
+    return (
+      <Profile
+        collapsible={collapsible}
+        onSwitchTheme={this.onSwitchTheme}
+        headerHeight={this.state.headerHeight}
+      />
+    );
   }
 }
 
-export default ProfileContainer;
+export default withCollapsible(ProfileContainer, {
+  iOSCollapsedColor: 'transparent',
+});
