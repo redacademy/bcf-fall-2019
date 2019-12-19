3841a0b679c217309bb118570e76984bca0726b5
Yongmin Kim <yongmin86k@gmail.com>
2019-12-18 21:25:46 -0800

    Update header style at Search and SubmitReview screen

--- client/js/screens/Search/SearchContainer.js
+++ client/js/screens/Search/SearchContainer.js
@@ -1,7 +1,39 @@
 import React, {Component} from 'react';
 import Search from './Search';
+import {TouchableOpacity, StatusBar, Image} from 'react-native';
+import styles from './styles';
+import {THEME} from '../../config';
 
 class SearchContainer extends Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+      headerStyle: {
+        backgroundColor: THEME.colors.white,
+      },
+
+      headerTitleStyle: {
+        color: THEME.colors.astronautBlue,
+      },
+
+      headerBackground: () => {
+        return <StatusBar barStyle="dark-content" />;
+      },
+
+      headerRight: () => (
+        <TouchableOpacity
+          style={styles.icMenu}
+          onPress={() => {
+            navigation.toggleDrawer();
+          }}>
+          <Image
+            source={require('../../assets/images/icMenuDefault.png')}
+            name="burger-menu"
+          />
+        </TouchableOpacity>
+      ),
+    };
+  };
+
   render() {
     return <Search />;
   }
